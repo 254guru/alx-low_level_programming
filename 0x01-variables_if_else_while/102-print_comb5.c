fec855c1f67419898dcb4aa99abfa5711a600e85
254guru <otienooluda@gmail.com>
2023-02-19 11:47:44 -0800

    different combs3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,12 +18,12 @@ int main(void)
 			{
 				putchar('0' + i / 10);
 				putchar('0' + i % 10);
-				putchar(' ');
 				putchar('0' + j / 10);
 				putchar('0' + j % 10);
 				putchar(',');
 				putchar(' ');
 			}
 	}
+	putchar('\n')
 	return (0);
 }
