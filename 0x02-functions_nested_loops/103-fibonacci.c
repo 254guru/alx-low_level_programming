458f819cd53e456ccb0d1019224eaefb980cf899
254guru <otienooluda@gmail.com>
2023-02-22 11:02:00 -0800

    corrected betty

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,19 +8,19 @@
 
 int main(void)
 {
-    	int a = 1, b = 2, c, sum = 0;
+	int a = 1, b = 2, c, sum = 0;
 
-    	while (b <= 4000000) 
-    	{
-        if (b % 2 == 0) 
+	while (b <= 4000000)
 	{
-            sum += b;
-        }
-        c = a + b;
-        a = b;
-        b = c;
-    	}
+		if (b % 2 == 0)
+		{
+			sum += b;
+		}
+		c = a + b;
+		a = b;
+		b = c;
+	}
 
-    	printf("%d\n", sum);
-    	return 0;
+	printf("%d\n", sum);
+	return (0);
 }
