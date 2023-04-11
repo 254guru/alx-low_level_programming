70ed713239878cf19bfaff1039f23e654af437f8
254guru <otienooluda@gmail.com>
2023-04-10 20:07:21 -0700

    copies content of another file to another

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,6 +11,9 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
+void print_error_and_exit(const char *filename, const char *message);
+void print_usage_and_exit();
 int _putchar(char c);
 
 #endif /* MAIN_H */
