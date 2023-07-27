146a9196143b1c54ca15adba36fe186510b8cea2
254guru <otienooluda@gmail.com>
2023-07-27 00:41:37 -0700

    binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,77 @@
 #include "main.h"
+
 /**
- * binary_to_uint - converts a binary to unsigned int
- * @b: binary to convert
+ * string_length - function retruns the length of a string
+ * @b: Pointer to a string of 0 and 1 chars
  *
- * Return: converted number or 0
+ * Return: size of string
+ */
+int string_length(const char *b)
+{
+	int index, string_length;
+
+	string_length = 0;
+	if (b == 0)
+		return (string_length);
+	for (index = 0; b[index]; index++)
+		string_length++;
+	return (string_length);
+}
+
+/**
+ * power - function that returns the exponential power
+ * @b: base integer
+ * @xp: exponential integer
+ *
+ * Return: unsigned integer
+ */
+unsigned int power(int b, int xp)
+{
+	unsigned int total;
+
+	total = 1;
+	if (b < 0)
+		return (0);
+	if (xp == 0)
+		return (total);
+	while (xp > 0)
+	{
+		total *= b;
+		xp--;
+	}
+	return (total);
+}
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: Pointer to a string of 0 and 1 chars
+ *
+ * Return: unsigned number or 0 is not valid
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0;
+	int pos, slen;
+	unsigned int total;
 
-	unsigned int result = 0;
+	total = 0;
+	slen = string_length(b) - 1;
 
-	if (b == NULL)
+	if (b == 0)
 		return (0);
 
-	while (b[i] != '\0')
+	for (pos = slen;
+			pos >= 0;
+			pos--)
 	{
-		if (b[i] != '0' || b[i] == '1')
+		if (b[pos] == '1')
 		{
-			result = result << 1;
-			result += b[i] - '0';
-			i++;
+			total += power(2, (slen - pos));
 		}
-		else
+		else if (b[pos] != '0'
+				&& b[pos] != '1')
 		{
 			return (0);
 		}
 	}
-	return (result);
+	return (total);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,6 +7,8 @@
 #include <string.h>
 
 unsigned int binary_to_uint(const char *b);
+unsigned int power(int b, int xp);
+int string_length(const char *b);
 void print_binary(unsigned long int n);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
