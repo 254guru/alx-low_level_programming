3bef4504b0a2e649432f21e015a7ecad3101905a
254guru <otienooluda@gmail.com>
2023-12-13 14:33:11 -0800

    update linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,41 +2,6 @@
 #include "search_algos.h"
 
 /**
- * print_value_checked - simulate printing without using printf
- * @value: param value
- *
- * Return: print message
- */
-void print_value_checked(int value)
-{
-	char buffer[1024];
-	int i = 0;
-	int temp = value;
-	int j;
-
-	if (temp == 0)
-	{
-		buffer[i++] = '0';
-	}
-	else
-	{
-		while (temp != 0)
-		{
-			int digit = temp % 10;
-
-			buffer[i++] = digit + '0';
-			temp /= 10;
-		}
-	}
-
-	for (j = i - 1; j >= 0; j--)
-	{
-		putchar(buffer[j]);
-	}
-	putchar('\n');
-}
-
-/**
  * linear_search - searches for a value in an array of integers
  * using linear search algorithm
  * @array: pointer to first element of the array
@@ -45,25 +10,23 @@ void print_value_checked(int value)
  *
  * Return: first index otherwise -1
  */
-
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	size_t i = 0;
 
 	if (array == NULL)
-	{
 		return (-1);
-	}
 
-	for (i = 0; i < size; i++)
+	for (; i < size; ++i)
 	{
-		print_value_checked(array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
 		if (array[i] == value)
-		{
-			return (i);
-		}
+			break;
 	}
 
-	return (-1);
+	if (i == size)
+		return (-1);
+
+	return (i);
 }
